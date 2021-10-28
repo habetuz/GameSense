42fd8d38fa0c6b259b984766c4e7c1bc75addd4e
Marvin Fuchs <marfinn3@gmail.com>
2021-10-28 18:14:34 +0200

    v1.2.2
    Fix:
    - 'FileNotFound' exception due to not importing the right version of 'System.Runtime.CompilerServices.Unsafe'
    
    Change:
    - references to only include necessary assemblies

--- Transmitter.cs
+++ Transmitter.cs
@@ -105,6 +105,7 @@ namespace GameSense
                     Logger.Log("coreProps.json could not be deserialized. Restart the SteelSeries Engine.", LogType.Error);
                 }
                 restartTimer.Start();
+                throw ex;
             }
         }
 
